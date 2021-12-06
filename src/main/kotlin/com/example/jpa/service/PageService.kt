ce494ebf3e373b822e011b528029b35ad292c768
kaku-m <kaku-m@m.sus-g.co.jp>
2021-12-07 01:52:23 +0900

    API開発

--- src/main/kotlin/com/example/jpa/service/PageService.kt
+++ src/main/kotlin/com/example/jpa/service/PageService.kt
@@ -4,6 +4,7 @@ import com.example.jpa.repository.PageRepository
 import com.example.jpa.repository.SequenceRepository
 import com.example.jpa.entity.PageEntity
 import com.example.jpa.entity.SequenceEntity
+import java.util.Optional
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,12 +13,36 @@ class PageService(
     private val sequenceRepository: SequenceRepository
 ) {
 
-    fun list(): Iterable<PageEntity> {
+    fun findAll(): Iterable<PageEntity> {
         return pageRepository.findAll()
     }
 
-    fun createNew(): SequenceEntity {
-        return sequenceRepository.save(SequenceEntity())
+    fun findById(id: Int): Optional<PageEntity> {
+        return pageRepository.findById(id)
+    }
+
+    fun create(parentPageId: Int?, pageTitle: String): PageEntity {
+        return if (parentPageId == null) createNew(pageTitle) else createChild(parentPageId, pageTitle)
+    }
+
+    fun createNew(pageTitle: String): PageEntity {
+        val sequence: SequenceEntity = sequenceRepository.save(SequenceEntity())
+        val page = PageEntity()
+        page.id = sequence.id
+        page.path = "/${page.id}/"
+        page.title = pageTitle
+        return pageRepository.save(page)
+    }
+
+    fun createChild(parentPageId: Int, pageTitle: String): PageEntity {
+        val parentPageFindResult: Optional<PageEntity> = pageRepository.findById(parentPageId)
+        val parentPage: PageEntity = parentPageFindResult.get()
+        val sequence: SequenceEntity = sequenceRepository.save(SequenceEntity())
+        val page = PageEntity()
+        page.id = sequence.id
+        page.path = "${parentPage.path}${page.id}/"
+        page.title = pageTitle
+        return pageRepository.save(page)
     }
 
 }
