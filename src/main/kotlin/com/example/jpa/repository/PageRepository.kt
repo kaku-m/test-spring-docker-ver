fb2cfc1cee7d3b27f34bf53049274e8a58a6582c
kaku-m <kaku-m@m.sus-g.co.jp>
2021-12-10 02:05:20 +0900

    API開発

--- src/main/kotlin/com/example/jpa/repository/PageRepository.kt
+++ src/main/kotlin/com/example/jpa/repository/PageRepository.kt
@@ -6,22 +6,55 @@ import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.query.Param
+import java.util.*
 
 @Repository
 interface PageRepository: CrudRepository<PageEntity, Int> {
 
+    @Query(
+        """
+        SELECT p1 FROM PageEntity AS p1 LEFT OUTER JOIN PageEntity AS p2
+        ON p2.path = (SELECT MAX(path) FROM PageEntity WHERE p1.path LIKE CONCAT(path, '_%'))
+        WHERE p2.id = :id
+        """
+    )
+    fun findChildren(
+        @Param("id") id: Int,
+    ): Iterable<PageEntity>
+
+    @Query(
+        """
+        SELECT p1 FROM PageEntity AS p1 LEFT OUTER JOIN PageEntity AS p2
+        ON p1.path = (SELECT MAX(path) FROM PageEntity WHERE p2.path LIKE CONCAT(path, '_%'))
+        WHERE p2.id = :id
+        """
+    )
+    fun findParent(
+        @Param("id") id: Int,
+    ): Optional<PageEntity>
+
+    @Query(
+        """
+        SELECT p FROM PageEntity AS p
+        WHERE (SELECT path FROM PageEntity WHERE id = :id) LIKE CONCAT(p.path, '_%')
+        """
+    )
+    fun findAncestors(
+        @Param("id") id: Int,
+    ): Iterable<PageEntity>
+
     @Modifying
     @Query("UPDATE PageEntity SET title = :title, content = :content WHERE id = :id")
     fun update(
-        @Param("id") pageId: Int,
-        @Param("title") pageTitle: String,
+        @Param("id") id: Int,
+        @Param("title") title: String,
         @Param("content") content: String
     )
 
     @Modifying
-    @Query("DELETE FROM PageEntity WHERE path LIKE :path")
+    @Query("DELETE FROM PageEntity WHERE path LIKE CONCAT('%/', :id, '/%')")
     fun delete(
-        @Param("path") path: String
+        @Param("id") id: Int
     )
 
 }
